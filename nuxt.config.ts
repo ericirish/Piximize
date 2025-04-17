9a9cf0c0485c44e406bd5030268eb4e4309e6efa
Eric Irish <ecirish@gmail.com>
2025-04-17 13:06:05 -0500

    add pwa

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -11,7 +11,7 @@ export default defineNuxtConfig({
 
   app: {
     head: {
-      title: '',
+      title: 'Piximize',
       meta: [
         { hid: 'og:image', property: 'og:image', content: '' },
         { hid: 'og:title', property: 'og:title', content: '' },
@@ -53,7 +53,8 @@ export default defineNuxtConfig({
     'nuxt-gtag',
     '@nuxt/content',
     'vuetify-nuxt-module',
-    '@nuxt/fonts'
+    '@nuxt/fonts',
+    '@vite-pwa/nuxt'
   ],
 
   vuetify: {
@@ -90,6 +91,72 @@ export default defineNuxtConfig({
   // gtag: {
   //   id: 'G-'
   // },
+
+  pwa: {
+    strategies: 'generateSW',
+    registerType: 'autoUpdate',
+    manifest: {
+      name: 'Piximize',
+      short_name: 'Piximize',
+      description: 'An easy way to optimize your images',
+      theme_color: '#187bc0',
+      start_url: '/',
+      display: 'standalone',
+      icons: [
+        {
+          src: '/pwa/64.png',
+          sizes: '64x64',
+          type: 'image/png'
+        },
+        {
+          src: '/pwa/192.png',
+          sizes: '192x192',
+          type: 'image/png'
+        },
+        {
+          src: '/pwa/512.png',
+          sizes: '512x512',
+          type: 'image/png',
+          purpose: 'any'
+        }
+        // {
+        //   src: 'maskable-icon-512x512.png',
+        //   sizes: '512x512',
+        //   type: 'image/png',
+        //   purpose: 'maskable'
+        // }
+      ]
+    },
+    workbox: {
+      navigateFallback: '/',
+      runtimeCaching: [{
+        urlPattern: /^https:\/\//,
+        handler: 'NetworkFirst', // Always try network first
+        options: {
+          networkTimeoutSeconds: 10, // Only wait 10 seconds before falling back to cache
+          cacheName: 'api-cache',
+          expiration: {
+            maxEntries: 50, // Keep fewer entries
+            maxAgeSeconds: 60 * 60 * 2 // Cache for only 2 hours
+          }
+        }
+      }],
+      skipWaiting: true,
+      clientsClaim: true,
+      cleanupOutdatedCaches: true
+    },
+    client: {
+      installPrompt: true,
+      // you don't need to include this: only for testing purposes
+      // if enabling periodic sync for update use 1 hour or so (periodicSyncForUpdates: 3600)
+      periodicSyncForUpdates: process.env.NODE_ENV !== 'production' ? 20 : 3600
+    },
+    devOptions: {
+      enabled: process.env.NODE_ENV === 'production',
+      suppressWarnings: true,
+      type: 'module'
+    }
+  },
 
   components: {
     global: true,
